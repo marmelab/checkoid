080c15494f888c050e8147ff4fbec6e97eba5b0e
thiery <thiery@marmelab.com>
2020-05-25 16:18:19 +0200

    add Validator.check that run the task and convert it to a promise

--- Validator.js
+++ Validator.js
@@ -1,12 +1,14 @@
 const Task = require("./Task");
 
-// run is a function that return a Task of a Result
+// run is a function that return a Task of a Validation
 const Validator = (run) => ({
     run,
     and: (other) => Validator((x, key) => run(x, key).and(other.run(x, key))),
     or: (other) => Validator((x, key) => run(x, key).or(other.run(x, key))),
+    check: (x, key) => run(x, key).toPromise(),
 });
 
-Validator.fromFn = (fn) => Validator((x, key) => Task.fromFn(() => fn(x, key)));
+const validator = (fn) => Validator((x, key) => Task.fromFn(() => fn(x, key)));
 
 exports.Validator = Validator;
+exports.validator = validator;
--- Validator.spec.js
+++ Validator.spec.js
@@ -1,19 +1,19 @@
-const { Validator } = require("./Validator");
+const { validator } = require("./Validator");
 const Validation = require("./Validation");
 
-const isAbsent = Validator.fromFn((value, key) => {
+const isAbsent = validator((value, key) => {
     if (!!value) {
         return Validation.Invalid([`${key} can be absent`]);
     }
     return Validation.Valid(value);
 });
-const isEmail = Validator.fromFn((value, key) => {
+const isEmail = validator((value, key) => {
     if (/@/.test(value)) {
         return Validation.Valid(value);
     }
     return Validation.Invalid([`${key} must be an email`]);
 });
-const isPresent = Validator.fromFn((value, key) => {
+const isPresent = validator((value, key) => {
     if (!!value) {
         return Validation.Valid(value);
     }
@@ -22,35 +22,32 @@ const isPresent = Validator.fromFn((value, key) => {
 
 describe("Validator", () => {
     it("should allow to run validator returning Validation", async () => {
-        const validator = Validator.fromFn((value, key) => {
+        const validate = validator((value, key) => {
             if (/@/.test(value)) {
                 return Validation.Valid(value);
             }
             return Validation.Invalid([`${key} must be an email`]);
         });
 
-        const validValidation = await validator
-            .run("some@email.com", "email")
-            .toPromise();
+        const validValidation = await validate.check("some@email.com", "email");
         expect(validValidation.x).toBe("some@email.com");
 
-        const invalidValidation = await validator
-            .run("I will type whatever I want", "email")
-            .toPromise();
+        const invalidValidation = await validate.check(
+            "I will type whatever I want",
+            "email"
+        );
         expect(invalidValidation.x).toEqual(["email must be an email"]);
     });
 
     it("should allow to combine validator with and", async () => {
         const validValidation = await isPresent
             .and(isEmail)
-            .run("some@email.com", "email")
-            .toPromise();
+            .check("some@email.com", "email");
         expect(validValidation.x).toBe("some@email.com");
 
         const noValueValidation = await isPresent
             .and(isEmail)
-            .run("", "email")
-            .toPromise();
+            .check("", "email");
         expect(noValueValidation.x).toEqual([
             "email must be present",
             "email must be an email",
@@ -58,30 +55,24 @@ describe("Validator", () => {
 
         const invalidEmailValidation = await isPresent
             .and(isEmail)
-            .run("whatever", "email")
-            .toPromise();
+            .check("whatever", "email");
         expect(invalidEmailValidation.x).toEqual(["email must be an email"]);
     });
 
     it("should allow to combine validator with or", async () => {
         const emailValidation = await isEmail
             .or(isAbsent)
-            .run("some@email.com", "email")
-            .toPromise();
+            .check("some@email.com", "email");
         expect(emailValidation.x).toBe("some@email.com");
         expect(emailValidation.isValid).toBe(true);
 
-        const noValueValidation = await isEmail
-            .or(isAbsent)
-            .run("", "email")
-            .toPromise();
+        const noValueValidation = await isEmail.or(isAbsent).check("", "email");
         expect(noValueValidation.x).toBe("");
         expect(noValueValidation.isValid).toBe(true);
 
         const invalidEmailValidation = await isEmail
             .or(isAbsent)
-            .run("whatever", "email")
-            .toPromise();
+            .check("whatever", "email");
         expect(invalidEmailValidation.x).toEqual([
             "email must be an email",
             "email can be absent",
@@ -90,7 +81,7 @@ describe("Validator", () => {
     });
 
     it("should support async validator function", async () => {
-        const isPresentInDb = Validator.fromFn(async (id, key) => {
+        const isPresentInDb = validator(async (id, key) => {
             await new Promise((resolve) => {
                 setTimeout(resolve, 500);
             });
@@ -101,23 +92,19 @@ describe("Validator", () => {
             return Validation.Valid(id);
         });
 
-        const validValidation = await isPresentInDb
-            .run("200", "userId")
-            .toPromise();
+        const validValidation = await isPresentInDb.check("200", "userId");
         expect(validValidation.x).toEqual("200");
         expect(validValidation.isValid).toBe(true);
 
         const invalidValidation = await isPresent
             .and(isPresentInDb)
-            .run("404", "userId")
-            .toPromise();
+            .check("404", "userId");
         expect(invalidValidation.x).toEqual(["userId does not exists"]);
         expect(invalidValidation.isValid).toBe(false);
 
         const invalidValidation2 = await isPresent
             .and(isPresentInDb)
-            .run("", "userId")
-            .toPromise();
+            .check("", "userId");
         expect(invalidValidation2.x).toEqual(["userId must be present"]);
         expect(invalidValidation2.isValid).toBe(false);
     });
