a7a31bbc070e0b09adb75826e444295a92f0e5b4
thiery <thiery@marmelab.com>
2020-06-13 18:14:22 +0200

    add index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+exports.validator = require("./Validator");
+exports.Validation = require("./validation");
+exports.objectValidator = require("./objectValidator");
+exports.listValidator = require("./listValidator");
