c9a9bef7ef095985c07b4765a30c790ff60c4e0f
thiery <thiery@marmelab.com>
2020-10-31 14:28:32 +0100

    add type to other validators

--- src/utils.ts
+++ src/utils.ts
@@ -9,24 +9,29 @@ export const and = <
     validator2: Validator<B>
 ) => validator1.and(validator2);
 
-export const path = (keys, obj) =>
+export const path = (keys: string[], obj: Object) =>
     keys.reduce((acc, key) => acc && acc[key], obj);
 
 const isDefined = (value) => {
     return typeof value !== "undefined";
 };
 
-const keysToPath = (key1, key2) =>
+const keysToPath = (key1: string, key2: string[] | undefined): string[] =>
     [].concat(key1).concat(isDefined(key2) ? key2 : []);
 
-const normalizeMessage = (message) => {
+const normalizeMessage = (
+    message: string | { message: string; key?: string[]; value: any }
+): { message: string; key?: string[]; value?: any } => {
     if (typeof message === "string") {
         return { message };
     }
     return message;
 };
 
-export const addKeyToMessage = (key) => (msg, entry) => {
+export const addKeyToMessage = (key: string) => (
+    msg: string | { message: string; key?: string[]; value: any },
+    entry
+) => {
     const message = normalizeMessage(msg);
     const newKey = keysToPath(key, message.key);
 
