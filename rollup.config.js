570ec78a62179ed05b76cb46c0d6e26eb98e9bb7
thiery <thiery@marmelab.com>
2020-11-01 10:50:19 +0100

    build to both commonjs and esmodule

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,18 @@
 import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
     input: "src/index.ts",
-    output: {
-        dir: "dist",
-        name: "checkoid",
-        format: "umd",
-    },
+    output: [
+        {
+            file: pkg.main,
+            format: "cjs",
+        },
+        {
+            file: pkg.module,
+            format: "es",
+        },
+    ],
     plugins: [
         typescript({
             typescript: require("typescript"),
