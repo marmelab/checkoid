27377a58088d22c6719b989ae5785e6b36a3b075
thiery <thiery@marmelab.com>
2021-01-06 12:04:07 +0100

    convert open api string schema to validator

--- src/openapi/openapi.ts
+++ src/openapi/openapi.ts
@@ -1,6 +1,71 @@
 import * as SwaggerParser from "@apidevtools/swagger-parser";
+import { OpenAPIV3 } from "openapi-types";
 
-export const getOpenApiSchemas = async (path: String) => {
-    const schema = await SwaggerParser.parse(path);
+import {
+    isString,
+    date,
+    dateTime,
+    binary,
+    base64,
+    match,
+    oneOf,
+} from "../validators/string";
+
+type Schema = OpenAPIV3.SchemaObject;
+
+export const getOpenApiSchema = async (path: string) => {
+    const schema = (await SwaggerParser.parse(path)) as OpenAPIV3.Document;
+
     return schema.components.schemas;
+};
+
+export const schemaToValidator = (schema: Schema) => {
+    switch (schema.type) {
+        case "string": {
+            switch (schema.format) {
+                case "date-time":
+                    return isString.and(dateTime);
+                case "date":
+                    return isString.and(date);
+                case "binary":
+                    return isString.and(binary);
+                case "byte":
+                    return isString.and(base64);
+                default: {
+                    if (schema.enum) {
+                        isString.and(oneOf(schema.enum));
+                    }
+                    if (schema.pattern) {
+                        isString.and(match(new RegExp(schema.pattern)));
+                    }
+                    return isString;
+                }
+            }
+        }
+        case "array":
+        case "boolean":
+        case "integer":
+        case "number":
+        case "object":
+    }
+};
+
+export const openApiValidator = async (path: string) => {
+    const schemas = await getOpenApiSchema(path);
+
+    Object.keys(schemas).reduce((acc, key) => {
+        const schema = schemas[key] as Schema;
+        switch (schema.type) {
+            case "array":
+            case "boolean":
+            case "integer":
+            case "number":
+            case "object":
+            case "string":
+        }
+        return {
+            ...acc,
+            [key]: schema,
+        };
+    }, {});
 };
--- src/validators/string.ts
+++ src/validators/string.ts
@@ -7,3 +7,21 @@ export const isString = validator(
 
 export const match = (pattern: RegExp) =>
     validator((value) => pattern.test(value), `value match pattern ${pattern}`);
+
+const dateTimeRe = /^([\+-]?\d{4}(?!\d{2}\b))((-?)((0[1-9]|1[0-2])(\3([12]\d|0[1-9]|3[01]))?|W([0-4]\d|5[0-2])(-?[1-7])?|(00[1-9]|0[1-9]\d|[12]\d{2}|3([0-5]\d|6[1-6])))([T\s]((([01]\d|2[0-3])((:?)[0-5]\d)?|24\:?00)([\.,]\d+(?!:))?)?(\17[0-5]\d([\.,]\d+)?)?([zZ]|([\+-])([01]\d|2[0-3]):?([0-5]\d)?)?)?)?$/;
+export const dateTime = match(dateTimeRe);
+
+const dateRe = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+export const date = match(dateRe);
+
+const base64Re = /[^-A-Za-z0-9+/=]|=[^=]|={3,}$/;
+export const base64 = match(base64Re);
+
+const binaryRe = /^[0-1]{1,}$/;
+export const binary = match(binaryRe);
+
+export const oneOf = (values: string[]) =>
+    validator(
+        (value) => (values.indexOf(value) !== -1 ? true : false),
+        `Value is one of ${values.join(", ")}`
+    );
